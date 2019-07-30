16b62ce712786f672bc25db68e6196aaaa741647
chenkj <chenkj@liby.com.cn>
2019-07-30 15:35:00 +0800

    1. 更新爬虫，修复requests请求数过多的问题，使用request session代替 2. 更新分析逻辑，pandas直接生成饼图

--- job_analyse.py
+++ job_analyse.py
@@ -2,58 +2,91 @@
 # --coding:utf-8--
 
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 
-# from matplotlib import font_manager
-# a = sorted([f.name for f in font_manager.fontManager.ttflist])
-# for i in a:
-#     print(i)
+#print(matplotlib.matplotlib_fname())
+job_list = pd.read_csv('job_item.csv', dtype={'职位名称': str, '公司名称': str, '地区': str, '薪酬': float, '工作经验': str,
+                                              '学历': str, '公司性质': str})
+#print(job_list['薪酬'].sum())
+#print(job_list['薪酬'].count())
+print('平均薪酬：', job_list['薪酬'].mean(), '元')
 
-print(matplotlib.matplotlib_fname())
-job_list = pd.read_csv('job_item.csv', dtype={'职位名称': str, '公司名称': str, '地区': str, '薪酬': float, '工作经验': str, '学历': str, '公司性质': str})
-print(job_list['薪酬'].sum())
-print(job_list['薪酬'].mean())
-print(job_list.groupby('工作经验').size())
-print(job_list.groupby('学历').size())
-print(job_list.groupby('地区').size())
-print(job_list.groupby('公司性质').size())
+#学历统计
+education_dic = dict(job_list.groupby('学历').size())
+key_list = []
+key_list.extend(education_dic.keys())
 
-exit()
+for key in key_list:
+    if '招' in key:
+        del education_dic[key]
 
-
-area_data = pd.read_csv('area.csv', dtype={'地区': str, '招聘数量': int})
-index = area_data['地区']
-values = area_data['招聘数量']
-plt.bar(index, values)
+education_labels = education_dic.keys()
+education_values = education_dic.values()
+plt.pie(education_values, labels=education_labels, autopct='%1.1f%%', shadow=False, startangle=150)
+plt.title("学历统计")
 plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
 plt.show()
 
+#工作经验统计
+experience_dic = dict(job_list.groupby('工作经验').size())
+key_list = []
+key_list.extend(experience_dic.keys())
+#去掉不包含年的工作经验选项
+for key in key_list:
+    if '年' not in key:
+        del experience_dic[key]
+
+experience_labels = experience_dic.keys()
+experience_values = experience_dic.values()
+plt.pie(experience_values, labels=experience_labels, autopct='%1.1f%%', shadow=False, startangle=150)
+plt.title("工作经验统计")
+plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
+plt.show()
+
+#工作地区统计
+area_dict = dict(job_list.groupby('地区').size())
+key_list = []
+key_list.extend(area_dict.keys())
+area_labels = area_dict.keys()
+area_values = area_dict.values()
+plt.pie(area_values, labels=area_labels, autopct='%1.1f%%', shadow=False, startangle=150)
+plt.title("工作地区统计")
+plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
+plt.show()
+
+#公司性质统计
+company_dic = dict(job_list.groupby('公司性质').size())
+key_list = []
+key_list.extend(company_dic.keys())
+
+company_labels = company_dic.keys()
+company_values = company_dic.values()
+plt.pie(company_values, labels=company_labels, autopct='%1.1f%%', shadow=False, startangle=150)
+plt.title("公司性质统计")
+plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
+plt.show()
+
+
+# print(job_list.groupby('学历').size())
+# print(job_list.groupby('公司性质').size())
 # education_data = pd.read_csv('education.csv', dtype={'学历': str, '招聘数量': int})
-# index = education_data['学历']
-# values = education_data['招聘数量']
-# plt.bar(index, values)
-# plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签  # 用来正常显示中文标签
+# plt.figure(figsize=(6, 9))
+# plt.pie(education_data['招聘数量'], labels=education_data['学历'], autopct='%.2f')
+# plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
 # plt.show()
-
-education_data = pd.read_csv('education.csv', dtype={'学历': str, '招聘数量': int})
-plt.figure(figsize=(6, 9))
-plt.pie(education_data['招聘数量'], labels=education_data['学历'], autopct='%.2f')
-plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
-plt.show()
-
-
-experience_data = pd.read_csv('experience.csv', dtype={'经验': str, '招聘数量': int})
-plt.figure(figsize=(6, 9))
-plt.pie(experience_data['招聘数量'], labels=experience_data['经验'], autopct='%.2f')
-plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
-plt.show()
-
-company_data = pd.read_csv('company.csv', dtype={'公司性质': str, '招聘数量': int})
-plt.figure(figsize=(6, 9))
-plt.pie(company_data['招聘数量'], labels=company_data['公司性质'], autopct='%.2f')
-plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
-plt.show()
+#
+#
+# experience_data = pd.read_csv('experience.csv', dtype={'经验': str, '招聘数量': int})
+# plt.figure(figsize=(6, 9))
+# plt.pie(experience_data['招聘数量'], labels=experience_data['经验'], autopct='%.2f')
+# plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
+# plt.show()
+#
+# company_data = pd.read_csv('company.csv', dtype={'公司性质': str, '招聘数量': int})
+# plt.figure(figsize=(6, 9))
+# plt.pie(company_data['招聘数量'], labels=company_data['公司性质'], autopct='%.2f')
+# plt.rcParams['font.sans-serif'] = ['Heiti TC'] #用来正常显示中文标签
+# plt.show()
 
 
 
--- job_spider.py
+++ job_spider.py
@@ -2,6 +2,8 @@
 # --coding:utf-8--
 
 import requests
+from requests.adapters import HTTPAdapter
+
 from lxml import etree
 import re
 from urllib.parse import urlparse
@@ -10,9 +12,12 @@ import csv
 
 DOMAIN = "jobs.51job.com"
 current_page = 1            #当前页数
-req_url = 'https://search.51job.com/list/030200,000000,0000,00,9,99,python,2,{0}.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0,0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+req_url = 'https://search.51job.com/list/030200,000000,0000,00,9,99,python,2,{0}.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
 total_page = float('inf')   #定义无限大
 has_next_page = True
+
+session = requests.Session()
+session.keep_alive = False
 
 headers = {
     "Host": "search.51job.com",
@@ -26,73 +31,12 @@ headers = {
 }
 
 
-def get_personal_requirement(url):
-    '''获取个人要求：学历，工作经验相关'''
-    res = requests.get(url, headers=headers)
-    res.encoding = 'gbk'
-    html = etree.HTML(res.text)
-    requirement = html.xpath("//div[@class='tHeader tHjob']//p[@class='msg ltype']/text()")
-    if requirement is not None and len(requirement) > 0:
-        return requirement[1].strip('\xa0'), requirement[2].strip('\xa0')
-
-
-def get_company_info(url):
-    '''获取公司信息：公司性质，人数，行业'''
-    res = requests.get(url, headers=headers)
-    res.encoding = 'gbk'
-    html = etree.HTML(res.text)
-    info = html.xpath("//div[@class='tHeader tHCop']//p[@class='ltype']/@title")
-    if info is not None and len(info) > 0:
-        return info[0].split('|')[0].strip('\r\n\t\t\t').strip('\xa0')
-    return ''
-
-
-def get_total_page(html):
-    '''根据页面信息，返回总共有多少页面'''
-    global total_page
-    result = html.xpath("//div[@class='dw_page']//div[@class='p_in']/span[@class='td'][1]/text()")
-    total_page = int(re.findall(r"\d+\.?\d*", result[0])[0])
-
-
-def has_next_page():
-    '''查看是否还有下一个页面'''
-    global current_page, total_page
-    return True if current_page <= total_page else False
-
-def transfer_salary(salary_str):
-    '''转换薪酬'''
-    salary_num = float(re.findall(r"\d+\.?\d*", salary_str)[0])
-    if '/' in salary_str:
-        if '月' in salary_str and '千' in salary_str:
-            return salary_num * 1000
-        elif '月' in salary_str and '万' in salary_str:
-            return salary_num * 10000
-        elif '年' in salary_str and '万' in salary_str:
-            return salary_num * 10000 / 12
-        elif '年' in salary_str and '千' in salary_str:
-            return salary_num * 1000 / 12
-        else:
-            return 0.00
-    return 0.00
-
-def write_to_csv_header():
-
-    with open('job_item.csv', 'a') as f:
-        f_csv = csv.writer(f)
-        f_csv.writerow(['职位名称', '公司名称', '地区', '薪酬', '工作经验', '学历', '公司性质'])
-
-def write_to_csv(job_item_list):
-
-    with open('job_item.csv', 'a') as f:
-        f_csv = csv.writer(f)
-        f_csv.writerows(job_item_list)
-
-
 def start_crawl():
 
-    global current_page, total_page, req_url
+    global current_page, total_page, req_url, session
 
-    res = requests.get(req_url.format(current_page), headers=headers)
+    #res = requests.get(req_url.format(current_page), headers=headers)
+    res = session.get(req_url.format(current_page), headers=headers)
     res.encoding = 'gbk'
     html = etree.HTML(res.text)
 
@@ -152,10 +96,80 @@ def start_crawl():
         #row.append(company_info[2])
 
         job_item_list.append(row)
+        print('crawling item:', row)
 
     write_to_csv(job_item_list)
-    print('total page:', total_page, ' and current page:', current_page)
+    print('******* total page:', total_page, ' and current page:', current_page, '*******')
     current_page += 1
+
+
+def get_personal_requirement(url):
+    '''获取个人要求：学历，工作经验相关'''
+    print('crawling personal url:', url)
+    #res = requests.get(url, headers=headers)
+    res = session.get(url, headers=headers)
+    res.encoding = 'gbk'
+    html = etree.HTML(res.text)
+    requirement = html.xpath("//div[@class='tHeader tHjob']//p[@class='msg ltype']/text()")
+    if requirement is not None and len(requirement) > 0:
+        return requirement[1].strip('\xa0'), requirement[2].strip('\xa0')
+
+
+def get_company_info(url):
+    '''获取公司信息：公司性质，人数，行业'''
+    print('crawling company url:', url)
+    #res = requests.get(url, headers=headers)
+    res = session.get(url, headers=headers)
+    res.encoding = 'gbk'
+    html = etree.HTML(res.text)
+    info = html.xpath("//div[@class='tHeader tHCop']//p[@class='ltype']/@title")
+    if info is not None and len(info) > 0:
+        return info[0].split('|')[0].strip('\r\n\t\t\t').strip('\xa0')
+    return ''
+
+
+def get_total_page(html):
+    '''根据页面信息，返回总共有多少页面'''
+    global total_page
+    result = html.xpath("//div[@class='dw_page']//div[@class='p_in']/span[@class='td'][1]/text()")
+    total_page = int(re.findall(r"\d+\.?\d*", result[0])[0])
+
+
+def has_next_page():
+    '''查看是否还有下一个页面'''
+    global current_page, total_page
+    return True if current_page <= total_page else False
+
+
+def transfer_salary(salary_str):
+    '''转换薪酬'''
+    salary_num = float(re.findall(r"\d+\.?\d*", salary_str)[0])
+    if '/' in salary_str:
+        if '月' in salary_str and '千' in salary_str:
+            return salary_num * 1000
+        elif '月' in salary_str and '万' in salary_str:
+            return salary_num * 10000
+        elif '年' in salary_str and '万' in salary_str:
+            return salary_num * 10000 / 12
+        elif '年' in salary_str and '千' in salary_str:
+            return salary_num * 1000 / 12
+        else:
+            return 0.00
+    return 0.00
+
+
+def write_to_csv_header():
+
+    with open('job_item.csv', 'a') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(['职位名称', '公司名称', '地区', '薪酬', '工作经验', '学历', '公司性质'])
+
+
+def write_to_csv(job_item_list):
+
+    with open('job_item.csv', 'a') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerows(job_item_list)
 
 
 if __name__ == '__main__':
